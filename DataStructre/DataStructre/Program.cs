1386798db1670258c89521c534a964223f1621a1
Ranganathan Palanisamy <ranganathan.palanisamy@gmail.com>
2022-02-13 10:03:36 -0500

    New Project - Solution Data Structure Programs for Learns

---
+++ DataStructre/DataStructre/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataStructre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
