f561a7acbaee6323dacb0e2e2d4ba2d15f924f64
Ranganathan Palanisamy <ranganathan.palanisamy@gmail.com>
2022-02-13 13:12:21 -0500

    Remove Duplicate Characters in String

--- DataStructre/DataStructre/Program.cs
+++ DataStructre/DataStructre/Program.cs
@@ -6,7 +6,30 @@ namespace DataStructre
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Remove the Duplicate Value : " + RemoveDuplicateCharsInString("Remove Duplicate Value"));
+            Console.ReadLine();
+        }
+        static string RemoveDuplicateCharsInString(string inputStrValue)
+        {
+            // Store encountered letters in this string.
+            string tble = string.Empty;
+
+            // Store the result in this string.
+            string reValue = string.Empty;
+
+            // Loop over each character.
+            foreach ( char chrValue in inputStrValue)
+            {
+                // See if character is in the table.
+                if (tble.IndexOf(chrValue) == -1)
+                {
+                    // Append to the table and the result.
+                    tble += chrValue;
+                    reValue += chrValue;
+                }
+            }
+            return reValue;
         }
     }
 }
